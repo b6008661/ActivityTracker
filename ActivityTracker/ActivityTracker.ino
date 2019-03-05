fb4341774d8b5873181a705acc9233c66ad5bb66
Claire <b6008661@my.shu.ac.uk>
2019-03-05 12:47:10 +0000

    declaring accel

--- ActivityTracker/ActivityTracker.ino
+++ ActivityTracker/ActivityTracker.ino
@@ -1,8 +1,20 @@
 #include <SoftwareSerial.h>
+#include <Wire.h>
+/*Bluetooth module*/
 SoftwareSerial BT(3,2);
+
+/*Accelerometer*/
+#define MPU6050_I2C_ADDRESS 0x68
+
+/*variables*/
+#define SEND_TIME 1000
+#define READ_TIME 50
+unsigned long previousTime = 0;
+unsigned lng currentTime = 0;
 
 void setup() {
   Serial.begin(9600); //open the serial port
+  Wire.begin();
   BT.begin(9600); // open the bluetooth serial port
 }
 
@@ -13,4 +25,13 @@ void loop() {
   if(Serial.available()){
     BT.println(Serial.readString()); // send from bluetooth to serial
   }
+}
+
+/*Bluetooth connection */
+void sendToApp() {
+  
+}
+
+void retrieveFromApp() {
+  
 }
